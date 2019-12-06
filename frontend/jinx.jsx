84d82f033cd0646d30c49589899573deb62400db
Justin Meyer <meyerj.justin@yahoo.com>
2019-12-05 19:28:01 -0500

    setup pt.2

---
+++ frontend/jinx.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import Root from './component/root';
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  const root = document.getElementById('root');
+  ReactDOM.render(Root, root);
+});
