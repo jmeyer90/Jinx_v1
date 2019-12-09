a7b329f8b04021265195811f419547fabee5b28b
Justin Meyer <meyerj.justin@yahoo.com>
2019-12-09 13:52:39 -0500

    completed user model auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,44 @@
 #  #<ActiveRecord::ConnectionAdapters::PostgreSQL::TableDefinition :integer
 #
 
+require 'securerandom'
+
 class User < ApplicationRecord
+
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, :f_name, :l_name, presence: true
+  validates :password, length:{minimum: 6}, allow_nil: true
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials( email, password )
+    user = User.find_by( email: email )
+    return nil unless user && user.is_password?( password )
+    user
+  end
+
+  def password=( password )
+    @password = password
+    self.password_digest = BCrypt::Password.create( password )
+  end
+
+  def is_password?( password )
+    bcp = BCrypt::Password( self.password_digest )
+    bcp.is_password?( password )
+  end
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
 end
